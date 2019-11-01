018088794aff16b6f42bcdf069bb77c14b160ec4
tlaudahl <travis.laudahl@gmail.com>
2019-11-01 11:59:05 -0500

    Finished action routes

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const projectRoute = require('./routes/projectRoutes');
+const actionRoute = require('./routes/actionRoutes');
 
 const server = express();
 
 server.use('/projects', projectRoute);
+server.use('/actions', actionRoute);
 
 server.listen(4000, () => {
     console.log("\n === Server listening on port 4000 === \n");
