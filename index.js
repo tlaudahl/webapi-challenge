581f0720c8c8da53fa6bd044e045938797775e60
tlaudahl <travis.laudahl@gmail.com>
2019-11-01 10:54:59 -0500

    CRUD for projects complete along with a projects route - working on actions CRUD next

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-/*
-play this: https://www.youtube.com/watch?v=d-diB65scQU
+const express = require('express');
+const projectRoute = require('./routes/projectRoutes');
 
-Sing along:
+const server = express();
 
-here's a little code I wrote, you might want to read it really slow, don't worry be happy
-in every line there may be trouble, but if you worry you make it double, don't worry, be happy
-ain't got no sense of what is REST? just concentrate on learning Express, don't worry, be happy
-your file is getting way too big, bring a Router and make it thin, don't worry, be crafty
-there is no data on that route, just write some code, you'll sort it out… don't worry, just API…
-I need this code, just don't know where, perhaps should make some middleware, don't worry, be happy
+server.use('/projects', projectRoute);
 
-Go code!
-*/
+server.listen(4000, () => {
+    console.log("\n === Server listening on port 4000 === \n");
+})
